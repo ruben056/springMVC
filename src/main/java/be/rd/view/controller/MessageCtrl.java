5aee4eabcb7d2e76e9d0730dab0517f629ed1b3a
ruben <demuynck_ruben@hotmail.com>
2015-12-27 13:10:21 +0100

    Added operation on controller to let the client send the message to be shown as a "pathvariable"

--- src/main/java/be/rd/view/controller/MessageCtrl.java
+++ src/main/java/be/rd/view/controller/MessageCtrl.java
@@ -2,6 +2,7 @@ package be.rd.view.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MessageCtrl {
 
 	private static final String VIEW_SHOW_MSG ="msg/showMessage";
+	
+	@RequestMapping(value = "/{msg}", method = RequestMethod.GET)
+	public String showMessage(@PathVariable("msg") String msg, Model uiModel){
+		uiModel.addAttribute("message", msg);
+		return VIEW_SHOW_MSG;
+	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showMessage(Model uiModel){
